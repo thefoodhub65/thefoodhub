71b11933abea5f85a63eabed2c7ec891e19c14b6
Hardik5862 <67375963+Hardik5862@users.noreply.github.com>
2021-11-21 18:46:59 +0530

    adjusting category cards

--- components/homepage/category-card.js
+++ components/homepage/category-card.js
@@ -14,8 +14,8 @@ const CategoryCard = ({ imgUrl, title, desp, loc }) => {
     <Card className={classes.root}>
       <CardActionArea onClick={() => router.push(loc)}>
         <Image src={imgUrl} alt={title} width="485" height="497" />
-        <CardContent>
-          <Typography color="primary" gutterBottom variant="h5" component="h2">
+        <CardContent className={classes.content}>
+          <Typography className={classes.title} variant="body2" component="p">
             {title}
           </Typography>
         </CardContent>
@@ -28,13 +28,21 @@ const useStyles = makeStyles((theme) => ({
   root: {
     maxWidth: 340,
     transition: "all 0.5s ease",
+    cursor: "pointer",
+    borderRadius: "20px",
+    boxShadow: "rgba(0, 0, 0, 0.60) 0px 4px 12px",
     "&:hover": {
-      transform: "scale(1.05) rotate(-3deg)",
+      transform: "scale(1.05) rotate(3deg)",
     },
   },
-  // media: {
-  //   height: 140,
-  // },
+  title: {
+    fontFamily: "Amita, cursive",
+    fontSize: "1.2rem",
+    color: "white",
+  },
+  content: {
+    backgroundColor: theme.palette.primary.main,
+  },
 }));
 
 export default CategoryCard;
