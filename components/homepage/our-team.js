867546e8d537db1df510f6b941da48e7a268e6a9
Hardik5862 <67375963+Hardik5862@users.noreply.github.com>
2021-11-24 22:19:40 +0530

    yet another team member

--- components/homepage/our-team.js
+++ components/homepage/our-team.js
@@ -31,6 +31,13 @@ const OurTeam = () => {
             desp="Chef"
           />
         </Grid>
+        <Grid item className={classes.gridItem}>
+          <TeamCard
+            imgUrl="/images/team/manage1.png"
+            name="Mr. Gautam Gupta"
+            desp="Managing Staff"
+          />
+        </Grid>
       </Grid>
     </div>
   );
